7d7d3f933a7a979a9808ae1c74fb98976199e05c
FATHURRAHMAN PRATAMA <fathurrahman.pratama49@smk.belajar.id>
2026-05-07 12:35:45 +0700

    .

--- koneksi/koneksi.php
+++ koneksi/koneksi.php
@@ -7,7 +7,7 @@ $db   = "tugas_blogspot";
 $conn = mysqli_connect($host, $user, $pass, $db);
 
 if (!$conn) {
-    die("ah cing baleg atuh nga konekeun teh!:" .mysqli_connect_error())
+    die("ah cing baleg atuh nga konekeun teh!:" .mysqli_connect_error());
 }
 
 ?>
